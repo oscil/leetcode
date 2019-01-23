58e6800d361cbe996b99b7341570d8693c80f5e3
Oscil <oscillatorium@gmail.com>
2019-01-23 21:10:10 +0300

    Cool solution with O(n) hash map/

---
+++ two_sum.py
@@ -0,0 +1,35 @@
+import random
+
+
+# -----------------------------
+
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        d = dict()
+        for i, e in enumerate(nums):
+            result = target - e
+            # if result in d:
+            if result in d:
+                return [d[result], i]
+            d[e] = i
+
+
+
+# ------------------------
+if __name__ == '__main__':
+    nums = list(range(20))
+    random.shuffle(nums)
+    s = Solution()
+
+    nums = [-3, 4, 3, 90]
+    target = 0
+
+    print(nums)
+    out = s.twoSum(nums, target)
+    print(out)
+    print([nums[out[0]], nums[out[1]]])
